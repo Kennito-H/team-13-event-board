6e79cda9790eb74d74c06e01db11a89b194644bb
Kennito-H <kennyhoh2014@gmail.com>
2026-04-15 19:05:34 -0400

    feat: extended EventRespository interface to findAll()

--- src/repository/EventRepository.ts
+++ src/repository/EventRepository.ts
@@ -3,4 +3,5 @@ import type { Event } from '../events/Event';
 export interface IEventRepository {
   findById(id: string): Promise<Event | null>;
   save(event: Event): Promise<Event>;
+  findAll(): Promise<Event[]>
 }
